81dd60707803d7158c53ae175920fc02d12e2956
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2021-09-21 10:49:34 -0500

    01 github

---
+++ 01_github.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import json # to parse JSON
+import requests # to make HTTP requests
+import argparse # to parse command-line arguments
+
+if __name__ == "__main__":
+    print("Hello World!")
